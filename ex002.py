2aa1588f8931144240fa316652292a09af52d68f
matheus.guerra <matheusguerra@outlook.com>
2021-12-13 16:09:16 -0300

    add nascimento

--- ex002.py
+++ ex002.py
@@ -6,6 +6,10 @@ nome = input('Digite seu nome: ')
 print('Bem-vindo', nome)
 print('Bem-vindo {}!'.format(nome))
 
+dia = input('Digite o dia de nascimento: ')
+mes = input('digite o mes de nascimento: ')
+ano = input('Digite o ano de nascimento: ')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano, '. Correto?')
 
 # input de inteiros
 print('Digite sua idade e seu peso:')
@@ -13,7 +17,7 @@ idade, peso = map(int, sys.stdin.readline().split())
 print(nome, 'sua idade é', idade, 'seu peso é', peso)
 
 # input de arquivo txt
-arquivo = open('input_txt.txt', 'r', encoding = 'utf8')
+arquivo = open('input_txt.txt', 'r', encoding='utf8')
 conteudo = arquivo.read()
 print(conteudo)
 
